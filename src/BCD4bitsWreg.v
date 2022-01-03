8542e93fb70b380e9a66507c113a7a034bf5b1e5
Diego Sánchez <42362277+Diegoapp55@users.noreply.github.com>
2022-01-03 17:36:03 -0500

    Update BCD4bitsWreg.v

--- src/BCD4bitsWreg.v
+++ src/BCD4bitsWreg.v
@@ -40,6 +40,43 @@ always @(posedge enable) begin
   		case (num1)
         4'ha:
           case (count)
+            2'h2: begin bcd <= 4'h0;   an<=4'b1011; end
+            2'h3: begin bcd <= 4'h1;   an<=4'b0111; end
+          endcase
+        4'hb:
+          case (count)
+            2'h2: begin bcd <= 4'h1;   an<=4'b1011; end
+            2'h3: begin bcd <= 4'h1;   an<=4'b0111; end
+          endcase
+        4'hc:
+          case (count)
+            2'h2: begin bcd <= 4'h2;   an<=4'b1011; end
+            2'h3: begin bcd <= 4'h1;   an<=4'b0111; end
+          endcase
+        4'hd:
+          case (count)
+            2'h2: begin bcd <= 4'h3;   an<=4'b1011; end
+            2'h3: begin bcd <= 4'h1;   an<=4'b0111; end
+          endcase
+        4'he:
+          case (count)
+            2'h2: begin bcd <= 4'h4;   an<=4'b1011; end
+            2'h3: begin bcd <= 4'h1;   an<=4'b0111; end
+          endcase
+        4'hf:
+          case (count)
+            2'h2: begin bcd <= 4'h5;   an<=4'b1011; end
+            2'h3: begin bcd <= 4'h1;   an<=4'b0111; end
+          endcase
+        default:
+          case (count)
+            2'h2: begin bcd <= num1[3:0];   an<=4'b1011; end
+            2'h3: begin bcd <= 4'b0000;   an<=4'b0111; end
+          endcase
+      endcase
+      case (num2)
+        4'ha:
+          case (count)
             2'h0: begin bcd <= 4'h0;   an<=4'b1110; end
             2'h1: begin bcd <= 4'h1;   an<=4'b1101; end
           endcase
