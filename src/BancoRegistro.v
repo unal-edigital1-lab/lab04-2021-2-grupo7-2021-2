9e4ecab81790f2f8b7e3392f0caf546c64d2b4e2
Diego Sánchez <42362277+Diegoapp55@users.noreply.github.com>
2022-01-05 09:51:55 -0500

    Banco de Registro funcionando
    Agregué el proyecto para probar el módulo BancoRegistro y las respectivas modificaciones a los archivos dados para que funcionase en simulación e implementación.
    NOTA: Aún no he hecho la implementación, pero la simulación ya funciona al 100%.
    NOTA: Falta modificar el módulo maestro Lab4 con los dos submódulos BancoRegistro y 4bitsBCDWreg, para que funcione todo junto.

--- src/BancoRegistro.v
+++ src/BancoRegistro.v
@@ -1,58 +1,65 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    09:55:28 10/12/2019 
+// Company:
+// Engineer:
+//
+// Create Date:    09:55:28 10/12/2019
 // Design Name: 	 ferney alberto beltran
-// Module Name:    BancoRegistro 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Module Name:    BancoRegistro
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
 //
-// Dependencies: 
+// Dependencies:
 //
-// Revision: 
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
 module BancoRegistro #(      		 //   #( Parametros
-         parameter BIT_ADDR = 8,  //   BIT_ADDR Número de bit para la dirección
-         parameter BIT_DATO = 4  //  BIT_DATO  Número de bit para el dato
+         parameter BIT_ADDR = 8,  //   BIT_ADDR Número de bits para la dirección
+         parameter BIT_DATO = 4  //  BIT_DATO  Número de bits para el dato
 	)
 	(
-    input [BIT_ADDR-1:0] addrRa,
-    input [BIT_ADDR-1:0] addrRb,
-    
-	 output [BIT_DATO-1:0] datOutRa,
-    output [BIT_DATO-1:0] datOutRb,
-    
-	 input [BIT_ADDR:0] addrW,
-    input [BIT_DATO-1:0] datW,
-    
-	 input RegWrite,
+    input [BIT_ADDR-1:0] addrRa, //Recibe la dirección para leer el dato A
+    input [BIT_ADDR-1:0] addrRb, //Recibe la dirección para leer el dato B
+
+	  output [BIT_DATO-1:0] datOutRa, /*Entrega el dato A, guardado en la posición
+                                      addrRa*/
+    output [BIT_DATO-1:0] datOutRb, /*Entrega el dato A, guardado en la posición
+                                      addrRb*/
+
+	  input [BIT_ADDR:0] addrW, //Recibe la dirección en la cual cargará el dato
+    input [BIT_DATO-1:0] datW, //Recibe el dato a cargar
+
+	  input RegWrite, //Habilita la carga o lectura
     input clk,
     input rst
     );
 
-// La cantdiad de registros es igual a: 
-localparam NREG = 2 ** BIT_ADDR;
-  
-//configiración del banco de registro 
-reg [BIT_DATO-1: 0] breg [NREG-1:0];
+// La cantidad de registros es igual a:
+localparam NREG = 2 ** BIT_ADDR; /*Es decir, 2^BIT_ADDR. Esto es así porque por
+                                   n bits que se tengan para indicar la
+                                   dirección, se pueden elegir 2^n direcciones
+                                   diferentes*/
 
+//Configuración del banco de registro
+reg [BIT_DATO-1: 0] breg [NREG-1:0]; /*Crea NREG registros en total, cada uno de
+                                       BIT_DATO bits*/
 
-assign  datOutRa = breg[addrRa];
-assign  datOutRb = breg[addrRb];
+assign  datOutRa = breg [addrRa]; /*Envía el dato guardado en addrRa a la salida
+                                   datOutRa*/
+assign  datOutRb = breg [addrRb]; /*Envía el dato guardado en addrRb a la salida
+                                   datOutRb*/
 
 always @(posedge clk) begin
-	if (RegWrite == 1)
-     breg[addrW] <= datW;
+	if (RegWrite == 1) /*Si vale 1, carga el dato en la posición indicada y si
+                       vale 0, lee el dato guardado en la posición indicada*/
+     breg [addrW] <= datW;
   end
 
 
 
 endmodule
-
--- src/TestBench.v
+++ src/TestBench.v
@@ -1,33 +1,33 @@
 `timescale 1ns / 1ps
 
 ////////////////////////////////////////////////////////////////////////////////
-// Company: 
+// Company:
 // Engineer:
 //
 // Create Date:   21:29:15 10/17/2019
 // Design Name:   BancoRegistro
 // Module Name:   C:/Users/UECCI/Documents/GitHub/SPARTAN6-ATMEGA-MAX5864/lab/lab07-BancosRgistro/bancoreg/src/TestBench.v
 // Project Name:  lab07-BancosRgistro
-// Target Device:  
-// Tool versions:  
-// Description: 
+// Target Device:
+// Tool versions:
+// Description:
 //
 // Verilog Test Fixture created by ISE for module: BancoRegistro
 //
 // Dependencies:
-// 
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 ////////////////////////////////////////////////////////////////////////////////
 
 module TestBench;
 
 	// Inputs
-	reg [3:0] addrRa;
-	reg [3:0] addrRb;
-	reg [4:0] addrW;
+	reg [7:0] addrRa;
+	reg [7:0] addrRb;
+	reg [8:0] addrW;
 	reg [3:0] datW;
 	reg RegWrite;
 	reg clk;
@@ -37,16 +37,19 @@ module TestBench;
 	wire [3:0] datOutRa;
 	wire [3:0] datOutRb;
 
+  //Contador para la lectura (De 8 bits por el tamaño de addrRa y addrRb)
+	reg [7:0] i;
+
 	// Instantiate the Unit Under Test (UUT)
 	BancoRegistro uut (
-		.addrRa(addrRa), 
-		.addrRb(addrRb), 
-		.datOutRa(datOutRa), 
-		.datOutRb(datOutRb), 
-		.addrW(addrW), 
-		.datW(datW), 
-		.RegWrite(RegWrite), 
-		.clk(clk), 
+		.addrRa(addrRa),
+		.addrRb(addrRb),
+		.datOutRa(datOutRa),
+		.datOutRb(datOutRb),
+		.addrW(addrW),
+		.datW(datW),
+		.RegWrite(RegWrite),
+		.clk(clk),
 		.rst(rst)
 	);
 
@@ -56,20 +59,26 @@ module TestBench;
 		addrRb = 0;
 		addrW = 0;
 		datW = 0;
-		RegWrite = 0;
+		RegWrite = 1; //Habilita la carga
 		clk = 0;
 		rst = 0;
 
-		// Wait 100 ns for global reset to finish
-		#100;
-      for (addrRa = 0; addrRa < 8; addrRa = addrRa + 1) begin
-			#5 addrRb=addrRa+8;
-			 $display("el valor de registro %d =  %d y %d = %d", addrRa,datOutRa,addrRb,datOutRb) ;
+		//Carga algunos datos sucesivos
+		for (addrW = 0; addrW < 16; addrW = addrW +1) begin
+			#2 datW = datW + 1;
+		end
+		#1 RegWrite = 0; //Habilita la lectura
+		//Lee los primeros 16 registros (en paralelo)
+		for (i = 0; i < 8; i = i + 1) begin
+			addrRa = i; //Lee del Reg_0 al Reg_7
+			addrRb = i + 8; //Lee del Reg_7 al Reg_15
+			#2 $display("El Valor de Registro %d = %d  y %d = %d", addrRa, datOutRa, addrRb, datOutRb);
     end
-	 
-		
-		
 	end
-      
+	//Emulación de los pulsos de reloj para que todo funcione
+	always #1 clk = ~clk;
+	initial begin: TEST_CASE
+		$dumpfile("TestBench.vcd");
+		#(56) $stop;
+ 	end
 endmodule
-
