aae624be6860d178dbd533cb6ebce21e40fbc475
Diego Sánchez <42362277+Diegoapp55@users.noreply.github.com>
2022-01-07 12:29:59 -0500

    Simulación para BancoRegistro corregida
    Corregí el error que tenía ese testbench cuando se le bajaba a 3 al tamaño de las direcciones.
    Agregué la posibilidad de inicializar el registro con un archivo "*.mem"
    Ya el testbench permite visualizar todas las funciones solicitadas para el banco de registro.
    NOTA: Falta el testbench para Lab4.v y su respectivo resultado de simulación

--- src/BancoRegistro.v
+++ src/BancoRegistro.v
@@ -19,7 +19,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module BancoRegistro #(      		 //   #( Parametros
-         parameter BIT_ADDR = 3,  // BIT_ADDR Número de bits para la dirección (Default: 8)
+         parameter BIT_ADDR = 8,  // BIT_ADDR Número de bits para la dirección
          parameter BIT_DATO = 4  // BIT_DATO  Número de bits para el dato
 	)
 	(
@@ -49,21 +49,26 @@ localparam NREG = 2 ** BIT_ADDR; /*Es decir, 2^BIT_ADDR. Esto es así porque por
 reg [BIT_DATO-1:0] breg [NREG-1:0]; /*Crea NREG registros en total, cada uno de
                                        BIT_DATO bits*/
 
-reg [BIT_DATO-1:0] cont = 0; //Contador para el reset (descomentar para probar)
+reg [BIT_DATO-1:0] cont = 0; //Contador para el reset
 
-assign  datOutRa = breg [addrRa]; /*Envía el dato guardado en addrRa a la salida
+assign datOutRa = breg [addrRa]; /*Envía el dato guardado en addrRa a la salida
                                    datOutRa*/
-assign  datOutRb = breg [addrRb]; /*Envía el dato guardado en addrRb a la salida
+assign datOutRb = breg [addrRb]; /*Envía el dato guardado en addrRb a la salida
                                    datOutRb*/
+
+initial begin
+$readmemh("Reg8.mem", breg); //Carga los números impares HEX del archivo Reg16
+end
 
 always @(posedge clk) begin
   if (rst == 0) begin
     for (cont = 0; cont < NREG; cont = cont + 1) begin
-      breg [cont] <= 0; //Ver por qué en simulación esto no sirve o el TB se enloquece :/
+      breg [cont] <= 0;
     end
-  end else if (RegWrite == 1) /*Si vale 1, carga el dato en la posición indicada y si
-                         vale 0, lee el dato guardado en la posición indicada*/
-     breg [addrW] <= datW;
+  end else if (RegWrite == 1) /*Si vale 1, carga el dato en la posición indicada
+                                y si vale 0, lee el dato guardado en la posición
+                                indicada*/
+      breg [addrW] <= datW;
 end
 
 endmodule
--- src/TestBench.v
+++ src/TestBench.v
@@ -37,11 +37,13 @@ module TestBench;
 	wire [3:0] datOutRa;
 	wire [3:0] datOutRb;
 
-  //Contador para la lectura (De 8 bits por el tamaño de addrRa y addrRb)
+  //Contador para la lectura (Del tamaño de las direcciones)
 	reg [2:0] i;
+	//Contador para la escritura ()
+	reg [3:0] j;
 
 	// Instantiate the Unit Under Test (UUT)
-	BancoRegistro uut (
+	BancoRegistro #(3,4) uut (
 		.addrRa(addrRa),
 		.addrRb(addrRb),
 		.datOutRa(datOutRa),
@@ -59,19 +61,38 @@ module TestBench;
 		addrRb = 0;
 		addrW = 0;
 		datW = 0;
-		RegWrite = 1; //Habilita la carga
+		RegWrite = 0;
 		clk = 0;
-		rst = 0;
-
+		rst = 1;
+		/*Lee el banco de 2 en 2 (en paralelo) para verificar la inicialización
+		previa de la memoria con el archivo Reg8.mem*/
+		$display("\nLectura de la inicializacion previa:");
+		for (i = 0; i < 4; i = i + 1) begin
+			addrRa = i; //Lee del Reg_0 al Reg_4
+			addrRb = i + 4; //Lee del Reg_4 al Reg_8
+			#2 $display("El Valor de Registro %d = %d  y %d = %d", addrRa, datOutRa, addrRb, datOutRb);
+    end
+		#2 RegWrite = 1; //Habilita la carga
 		//Carga algunos datos sucesivos
-		for (addrW = 0; addrW < 8; addrW = addrW +1) begin
-			#2 datW = datW + 1;
+		for (j = 0; j < 8; j = j + 1) begin
+			addrW = j;
+			datW = j;
+			#2;
 		end
 		#2 RegWrite = 0; //Habilita la lectura
-		//Lee los primeros 16 registros (en paralelo)
+		$display("\nLectura luego de la carga:");
+		//Lee el banco de 2 en 2
 		for (i = 0; i < 4; i = i + 1) begin
-			addrRa = i; //Lee del Reg_0 al Reg_7
-			addrRb = i + 4; //Lee del Reg_7 al Reg_15
+			addrRa = i;
+			addrRb = i + 4;
+			#2 $display("El Valor de Registro %d = %d  y %d = %d", addrRa, datOutRa, addrRb, datOutRb);
+    end
+		#1 rst = 0; //Borra el banco (Lo deja todo en 0000)
+		$display("\nLectura luego del reset:");
+		//Lee el banco de 2 en 2 para verificar el efecto del reset
+		for (i = 0; i < 4; i = i + 1) begin
+			addrRa = i;
+			addrRb = i + 4;
 			#2 $display("El Valor de Registro %d = %d  y %d = %d", addrRa, datOutRa, addrRb, datOutRb);
     end
 	end
@@ -79,6 +100,6 @@ module TestBench;
 	always #1 clk = ~clk;
 	initial begin: TEST_CASE
 		$dumpfile("TestBench.vcd");
-		#(56) $stop;
+		#(45) $stop;
  	end
 endmodule
